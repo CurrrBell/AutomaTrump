936d2aef6ff99392cbc5383c77ebde7bea81b514
Chris Bell <chrislbellny@gmail.com>
2016-12-24 22:38:59 -0500

    holy shit we tweeted for the first time

--- pullTweets.py
+++ pullTweets.py
@@ -1,14 +1,22 @@
 from twython import Twython
 
 def main():
-    APP_KEY = 'i5yVTADf633De8I7fgjXDh6SY'
-    APP_SECRET = 'fVJkx7NvVmZDtMSKDXvaPwfjrRhp2yUkwNfuEMJrYa1C4yKZfB'
+    APP_KEY = 'PtIsqf9tSAXqMO1EiwQ1yyWfT'
+    APP_SECRET = 'MCaPnJpc2q2bQJgm9yc2U275Tzu98WRyPv1H8tTFYsgfdoYLqC'
+    OAUTH_TOKEN = '812765448685154304-GEyoxEGDJzEJsaVLP5uyD4kXYbcY0e2'
+    OAUTH_TOKEN_SECRET = 'TnJEyf5pSLay3HqHQ677ocj7ADJMAoMK4y4Twms25kpiC'
 
-    twitter = Twython(APP_KEY, APP_SECRET)
+    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-    user_timeline = twitter.get_user_timeline(screen_name="realDonaldTrump",count=20)
-    for tweet in user_timeline:
-        if(tweet['source'] == "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>"):
-            print (tweet['text'])
-            print (tweet['source'])
-            print ()
+#    user_timeline = twitter.get_user_timeline(screen_name="realDonaldTrump",count=20)
+#    for tweet in user_timeline:
+#        if(tweet['source'] == "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>"):
+#            print (tweet['text'])
+#            print (tweet['source'])
+#            print ()
+
+
+#    for tweet in twitter.get_home_timeline():
+#        print(tweet['text'])
+
+    twitter.update_status(status='beep boop')        
